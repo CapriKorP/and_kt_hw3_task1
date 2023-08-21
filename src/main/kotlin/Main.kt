22dbfbf6e294733327305934642b0e3a11e1fdb9
pavel <gitkornienkov@gmail.com>
2023-08-21 23:40:09 +0300

    Final code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,38 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    var time = 87000
+    if (time >= 0) {
+        println("Был ${agoToText(time)}") //Выводим значение функции agoToText
+    } else {
+        println("ОШИБКА!!! Время не может быть отрицательным!!!") //Защитились от отризательных значений переменной time
+    }
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun agoToText(time: Int):String {
+    val string = when(time) { //значение положим в переменную
+        in 0..60 -> "только что"
+        in 61..60 * 60 -> "${time / 60} минут${minutesEnding(time)} назад" //просим от функции minutesEnding вернуть окончание
+        in 60 * 60 + 1 .. 24 * 60 * 60 -> "${time / (60 * 60)} чаc${hoursEnding(time)} назад"
+        in 24 * 60 * 60 + 1 .. 48 * 60 * 60 -> "вчера"
+        in 48 * 60 * 60 + 1 .. 78 * 60 * 60 -> "позавчера"
+        else -> {"давно"}
+    }
+    return string //возвращаем значение переменной
+}
+
+fun minutesEnding(time: Int): String {
+    val endMinutes = when (time / 60) {
+        1, 21, 31, 41, 51  -> "у"
+        2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54 -> "ы"
+        else -> ""
+    }
+    return endMinutes
+}
+
+fun hoursEnding(time: Int):String {
+    val endHours = when (time / 3600) {
+        2,3,4,22,23 -> "а"
+        in 5..20 -> "ов"
+        else -> ""
+    }
+    return endHours
 }
